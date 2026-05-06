5632b8d40c17c87bf9adc33f4b3903acca427f81
Piemoroni <moronipietra@gmail.com>
2026-05-06 14:27:38 -0300

    first commit

---
+++ src/controllers/roupas.controller.js
@@ -0,0 +1,43 @@
+const prisma = require("../data/prisma");
+
+const cadastrar = async (req, res) => {
+  const item = await prisma.roupas.create({ data: req.body });
+  res.json(item);
+};
+
+const listar = async (req, res) => {
+  const lista = await prisma.roupas.findMany();
+  res.json(lista);
+};
+
+const buscar = async (req, res) => {
+  const item = await prisma.roupas.findUnique({
+    where: { id: Number(req.params.id) }
+  });
+
+  if (!item) return res.json({ erro: "Roupa não encontrada" });
+
+  res.json(item);
+};
+
+const atualizar = async (req, res) => {
+  const item = await prisma.roupas.findUnique({
+    where: { id: Number(req.params.id) }
+  });
+
+  if (!item) return res.json({ erro: "Roupa não encontrada" });
+
+  const atualizado = await prisma.roupas.update({
+    where: { id: Number(req.params.id) },
+    data: req.body
+  });
+
+  res.json(atualizado);
+};
+
+module.exports = {
+  cadastrar,
+  listar,
+  buscar,
+  atualizar
+};
